6b80614dd29633bb829a1957e7127192f322612b
John-Mike <jmmarquardt@gmail.com>
2016-09-01 21:16:02 -0400

    firebase initialized

---
+++ assets/javascript/beer_app.js
@@ -0,0 +1,44 @@
+// load JS below after DOM is ready
+$(document).ready(function () {
+
+	// Initialize Firebase
+  	var config = {
+	    apiKey: "AIzaSyDzM95iQ57H9r9NbquI9eNky3bsNFQVfrU",
+	    authDomain: "beer-near-here.firebaseapp.com",
+	    databaseURL: "https://beer-near-here.firebaseio.com",
+	    storageBucket: "",
+	  };
+	  firebase.initializeApp(config);
+
+	var database = firebase.database();
+
+
+	// write your JS here
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  })
